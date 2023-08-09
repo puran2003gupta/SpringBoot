f9f676b85ef519f6bd174545bd6bc0e83a55c161
Puran kumar Gupta <109130056+puran2003gupta@users.noreply.github.com>
2023-08-09 23:08:59 +0530

    2nd approachtto link with database

--- src/main/java/com/springrest/springrest/SpringrestApplication.java
+++ src/main/java/com/springrest/springrest/SpringrestApplication.java
@@ -1,5 +1,6 @@
 package com.springrest.springrest;
 
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -7,19 +8,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.springrest.springrest.dao.CourseDao;
+import com.springrest.springrest.entities.Course;
 
 @SpringBootApplication
 public class SpringrestApplication implements CommandLineRunner {
-
 	@Autowired
-	private CourseDao courseDao;
+	private CourseDao studentDao;
 	public static void main(String[] args) {
 		SpringApplication.run(SpringrestApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		this.courseDao.createTable(); 
+		this.studentDao.createTable();
 		
 	}
 	
--- src/main/java/com/springrest/springrest/dao/CourseDao.java
+++ src/main/java/com/springrest/springrest/dao/CourseDao.java
@@ -6,14 +6,14 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class CourseDao {
-
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
-	
-	//create the table
-	public void createTable() {
-		var query ="CREATE TABLE COURSE(courseId SERIAL PRIMARY KEY, title VARCHAR(255), description VARCHAR(255))";
-		int update = this.jdbcTemplate.update(query);
-		System.out.println(update);
-	}
+public void createTable() 
+{
+	var query ="CREATE TABLE course(courseId SERIAL PRIMARY KEY, title VARCHAR(255), description VARCHAR(255))";
+	int update = this.jdbcTemplate.update(query);
+	System.out.println(update);
+}
+
+
 }
